3778eab1de5fb88cb6c8d81d514e35c34d846378
makkahwi <suhaibahmadai@hotmail.com>
2022-03-14 10:28:49 +0300

    Initial Strcuture

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,24 @@
+class GameLogic:
+  def __init__(self):
+    pass
+
+  def calculate_score(roll):
+    pass
+
+  def roll_dice(dices):
+    pass
+
+class Banker:
+  total = 0
+
+  def __init__(self):
+    pass
+
+  def shelf(temPoint):
+    pass
+
+  def bank(unbanked):
+    pass
+
+  def clear_shelf():
+    pass
